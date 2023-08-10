73f906313a31cfbd28afce3e24f30fdf7acb917e
Saksham Bisen <sakshambisen123@gmail.com>
2023-08-10 12:05:56 +0530

    Homepage Completed

--- src/components/UI/Homepage.jsx
+++ src/components/UI/Homepage.jsx
@@ -1,25 +1,47 @@
 import Container from 'react-bootstrap/Container';
-import Navbar from 'react-bootstrap/Navbar';
 import Button from "react-bootstrap/Button";
+import Row from 'react-bootstrap/esm/Row';
+import Col from 'react-bootstrap/esm/Col';
 import { useNavigate } from 'react-router-dom';
 import Card from 'react-bootstrap/Card';
-// import UserWorkflow
 
+import Userworkflow from "../../assets/Userworkflow.jpg";
+import Companyworkflow from "../../assets/Companyworkflow.jpg";
 import { useAuthContext } from '../../context/AuthContext';
+
 
 function HomePage() {
     const navigate = useNavigate();
     const { user, signOut } = useAuthContext();
   return (  
-    <Container fluid>
+    <Container fluid className='mt-4 p-1'>
+      <h3>Welcome to Ethereum Identity Database</h3>  
+      <h3>Biggest Database of Verified users on Ethereum Blockchain.</h3>
+      <hr/>
+      <h3>For Users</h3>
+      <h4>Get Yourself Verified so that applications using our database could recognize you!</h4>
+      <h3>For Companies</h3>
+      <h4>Use our Database to allow only verified user to onboard on your Ethereum Application</h4>
+      <br />
+      <h3><strong>Working (User WorkfLow)</strong></h3>
     <Card>
-      <Card.Img variant="top" src="assets/Userworkflow.jpg" />
-      <Card.Body>
-        <Card.Title>User Workflow</Card.Title>
-      </Card.Body>
-    </Card>
-      <Button onClick={() => { navigate('/verify/register/') }}>Register ETH Name</Button>
-      <Button onClick={() => { navigate('/verify/get/') }}>Get ETH Name</Button>
+      <Card.Img variant="top" src={Userworkflow} />
+      </Card>
+      <Row>
+        <Col sm="12" className='text-center p-3 m-5'> 
+          <Col className='mb-3'>
+            Register Yourself Now!
+          </Col>
+          <Col>
+            <Button onClick={() => { navigate('/start/registration/') }}>Start Registration</Button>
+          </Col>
+        </Col>
+      </Row>
+      <br />
+      <h3><strong>Working (Company WorkfLow)</strong></h3>
+    <Card>
+      <Card.Img variant="top" src={Companyworkflow} />
+      </Card>
     </Container>
   );
 }
